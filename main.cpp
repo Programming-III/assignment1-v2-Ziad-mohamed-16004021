9da0f926e5c8a735a67732e3a7e4be88be9a00bc
Ziad-mohamed-16004021 <ztharwat2018@gmail.com>
2025-11-11 09:29:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,18 @@ using namespace std;
 void student :: display(){
         cout<<name<<cout<<id;
     }
+void person :: getname(){
+return name;
+}
+
+void person :: getid(){
+return id;
+}
+
+person :: person(){
+this->name=name;
+this->id=id;
+}
 
 
 
@@ -22,6 +34,18 @@ void student :: display(){
 // ==================== Student Class Implementation ====================
 void student :: display(){
   cout<<yearLevel<<major;
+}
+void student :: getyearLevel(){
+return yearLevel;
+}
+
+void student :: getmajor{
+return major;
+}
+
+student :: person(){
+this->yearLevel = yearLevel;
+this->major = major;
 }
 
 
@@ -37,9 +61,27 @@ void student :: display(){
 
 // ==================== Course Class Implementation ====================
 
+void course :: getcoursename(){
+return coursename;
+}
 
+void course:: getcurrentstudents(){
+return currentstudents;
+}
 
+void course :: getcoursecode(){
+return coursecode;
+}
 
+void course:: getmaxstudents(){
+return maxstudents;
+}
+
+course : course(){
+this->coursename=coursename;
+this->currentstudents=currentstudents;
+
+}
 
 
 
