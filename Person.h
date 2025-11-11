dca3810ad3c1ee6b596973a854c044a857e2105b
Ziad-mohamed-16004021 <ztharwat2018@gmail.com>
2025-11-11 09:19:11 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -6,13 +6,15 @@
 using namespace std;
 class person{
 private:
+string name;
+int id;
 
 public:
 void addStudent(const Student& s);
 void displayCourseInfo();
 void setname();
 void getname();
-Person();
+Person(string name,int id);
 Person();
 };
 
