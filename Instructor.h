72e61b4c0d345494375f3df0eade25ee64bdf780
Ziad-mohamed-16004021 <ztharwat2018@gmail.com>
2025-11-11 09:05:15 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,10 +1,19 @@
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
+class instructor{
+private:
+string department;
+int experienceYears
+
+public:
+void display();
+
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
-
+void instructor :: display(){
+  cout<<department<<experienceYears;
+}
 
 
 
