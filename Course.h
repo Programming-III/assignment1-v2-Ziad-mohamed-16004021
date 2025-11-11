1460873d37ae774c8f2096f6b0cf164e13e28609
Ziad-mohamed-16004021 <ztharwat2018@gmail.com>
2025-11-11 09:12:17 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -4,7 +4,20 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
+class{
+private:
+string coursecode;
+string coursename;
+int maxstudents;
+Student* students;
+int currentstudents;
+
+public:
+void addStudent(const Student& s);
+void displayCourseInfo();
+};
+
+
 
 
 
