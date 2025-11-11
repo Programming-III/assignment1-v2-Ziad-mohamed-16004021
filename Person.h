817d4b155a40318810a51b47200dde48c7aeb343
Ziad-mohamed-16004021 <ztharwat2018@gmail.com>
2025-11-11 08:56:11 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,9 +1,25 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+class person{
+private:
+    string name;
+    int id;
+    
+public:
+   void display();
+#endif
+    
+    
+
 #include <string>
 using namespace std;
 
+void display(){
+        cout<<name<<cout<<id;
+    }
+
+};
 //#write Person class here
 
 
